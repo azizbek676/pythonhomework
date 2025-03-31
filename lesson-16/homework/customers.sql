8d628d034a75e7592a5a77def11d7b526cf73eb6
azizbek676 <dilmurodovazizbek.2008@gmail.com>
2025-03-31 18:14:51 +0500

    task

---
+++ lesson-16/homework/customers.sql
@@ -0,0 +1,6 @@
+customer_id,first_name,last_name,email,country
+1,Ali,Valiyev,ali@example.com,Uzbekistan
+2,Madina,Karimova,madina@example.com,USA
+3,John,Doe,john@example.com,UK
+4,Sara,Smith,sara@example.com,Canada
+5,Bobur,Hasanov,bobur@example.com,Germany
